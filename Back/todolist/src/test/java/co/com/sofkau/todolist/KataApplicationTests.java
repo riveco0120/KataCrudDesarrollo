0ac2272e322bd9a6a9c6e2d83558276ef098cda0
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-03 21:40:39 -0500

    Iniciando back
    Se Inicia el repositorio de back

---
+++ Back/todolist/src/test/java/co/com/sofkau/todolist/KataApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
