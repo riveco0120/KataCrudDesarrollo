c6771065680c06bc0f0a737c130e839b94568a8d
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-07 17:35:55 -0500

    Actualizando back fton

--- Back/todolist/src/main/java/co/com/sofkau/todolist/ITodoRepository.java
+++
@@ -1,8 +0,0 @@
-package co.com.sofkau.todolist;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ITodoRepository  extends CrudRepository<Todo,Long>{
-}
--- Back/todolist/src/main/java/co/com/sofkau/todolist/ITodoService.java
+++
@@ -1,9 +0,0 @@
-package co.com.sofkau.todolist;
-
-
-public interface ITodoService {
-    public Iterable<Todo> list();
-    public Todo save(Todo tod);
-    public void delete(Long id);
-    public  Todo get(Long id);
-}
